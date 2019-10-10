90aa3004f2bce6af5c339ba0d02be344051076bb
matheeshaYapa <ylmatheesha@gmail.com>
2019-10-10 17:57:05 +0530

    created vehicle request form with a stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,7 @@ import {SharedModule} from './shared/shared.module';
 import { LayoutComponent } from './layout/layout.component';
 import { MainNavigationComponent } from './layout/main-navigation/main-navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { VehicleRequestComponent } from './pages/vehicle-request/vehicle-request.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,16 +23,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     SharedModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatInputModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/vehicle-request/vehicle-request.component.ts
+++ src/app/pages/vehicle-request/vehicle-request.component.ts
@@ -1,99 +1,65 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-request',
   templateUrl: './vehicle-request.component.html',
   styleUrls: ['./vehicle-request.component.scss']
 })
-export class VehicleRequestComponent implements OnInit{
+export class VehicleRequestComponent implements OnInit {
 
-  // vehicleRequestForm: FormGroup;
+  vehicleRequestForm: FormGroup;
 
-  vehicleRequestForm = this.fb.group({
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [null, Validators.compose([
-      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    ],
-    shipping: ['free', Validators.required]
-  });
+  personalDetailsForm: FormGroup;
+  flightDetailsForm: FormGroup;
+  vehicleDetailsForm: FormGroup;
 
-  hasUnitNumber = false;
+  get formArray(): AbstractControl | null {
+    return this.vehicleRequestForm.get('formArray');
+  }
 
-  states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
-  ];
-
-  constructor(private fb: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    // this.vehicleRequestForm = new FormGroup({
-    //   firstName: new FormControl('', {validators: [Validators.required]}),
-    //   lastName: new FormControl('', {validators: [Validators.required]}),
-    // });
+    this.vehicleRequestForm = this.formBuilder.group({
+      formArray: this.formBuilder.array([
+        this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          passportNo: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]]
+        }),
+        this.formBuilder.group({
+          arrivingFrom: ['', Validators.required],
+          arrivalDate: ['', Validators.required],
+          arrivalTime: ['', Validators.required],
+          flightNo: ['']
+        }),
+        this.formBuilder.group({
+          vehicleType: ['', Validators.required],
+          luxuryType: ['', Validators.required]
+        })
+      ])
+    });
+
+    this.personalDetailsForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      passportNo: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+    this.flightDetailsForm = this.formBuilder.group({
+      arrivingFrom: ['', Validators.required],
+      arrivalDate: ['', Validators.required],
+      arrivalTime: ['', Validators.required],
+      flightNo: ['']
+    });
+
+    this.vehicleDetailsForm = this.formBuilder.group({
+      vehicleType: ['', Validators.required],
+      luxuryType: ['', Validators.required]
+    });
   }
 
   onSubmit() {
